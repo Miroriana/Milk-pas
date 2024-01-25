0b255d91271f4f4a993ca848c0fa1ba5119c0d3f
miroriana <shumbushoihiclaudia@gmail.com>
2024-01-25 12:03:31 +0100

    true urls

--- server.js
+++ server.js
@@ -52,9 +52,11 @@ const options = {
     },
     servers: [
       {
-        url: "https://milk-production-analytical-system.onrender.com/",
-        url: "http://localhost:5678",
+        url: "https://milk-production-analytical-system.onrender.com/" ,
       },
+      {
+        url: "http://localhost:5678/",
+      }
     ],
   },
   apis: ["./routes/*.js"],
